d9697793f3ead058e9053d8a339bd25659167244
Naesa Orlina <norlina@thoughtworks.com>
2018-01-22 11:57:44 +1000

    NO: create NavBar component

---
+++ src/header/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-title">Welcome to GARAGE SALE APP</h1>
+            </header>
+        )
+    }
+}
+
+export default Navbar;
