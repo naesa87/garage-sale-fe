e780d986ab571fa1d4e14a325b6e10859481e719
Archana Khanal <archana@Archanas-MacBook-Pro.local>
2018-01-23 14:44:42 +1000

    NO/AK/DC Styled text

--- src/Listings/Listing.js
+++ src/Listings/Listing.js
@@ -14,8 +14,11 @@ class Listing extends Component {
                     <p className="listing-price"> {this.props.listing.price} <span className="currency">AUD</span> </p>
                 </div>
                 <div className="list-content">
+                <h3>Specs</h3>
                     <p> {this.props.listing.specs} </p>
+                    <h3>Condition</h3>
                     <p> {this.props.listing.condition} </p>
+                    <h3>Location</h3>
                     <p> {this.props.listing.location} </p>
                 </div>
             </div>
