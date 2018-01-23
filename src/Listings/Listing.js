8ed2abab48607b2b2271ac33e52c28c77a9e2674
Archana Khanal <archana@Archanas-MacBook-Pro.local>
2018-01-23 12:16:36 +1000

    NO/AK/DC Add styling for listing item square

--- src/Listings/Listing.js
+++ src/Listings/Listing.js
@@ -5,12 +5,14 @@ class Listing extends Component {
     render() {
         return (
             <div className="col-md-5 listing">
+                <div className="listing-upper">
                     <img src={logo} className="listing-image"/>
                     <div className="listing-heading">
                         <h2> {this.props.listing.title} </h2>
                         <p> {this.props.listing.serialNumber} </p>
-                    </div>
-                <p className="listing-price"> {this.props.listing.price} </p>
+                    </div> 
+                    <p className="listing-price"> {this.props.listing.price} </p>
+                </div>
                 <div className="list-content">
                     <p> {this.props.listing.specs} </p>
                     <p> {this.props.listing.condition} </p>
