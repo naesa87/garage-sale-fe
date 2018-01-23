2363ada57cac8371474ee75ee4d6dfeca5533583
Archana Khanal <archana@Archanas-MacBook-Pro.local>
2018-01-23 13:52:58 +1000

    NO/AK/DC Add styling for price

--- src/Listings/Listing.js
+++ src/Listings/Listing.js
@@ -6,12 +6,12 @@ class Listing extends Component {
         return (
             <div className="col-md-5 listing">
                 <div className="listing-upper">
-                    <img src={logo} className="listing-image"/>
                     <div className="listing-heading">
+                        <img src={logo} className="listing-image"/>
                         <h2> {this.props.listing.title} </h2>
                         <p> {this.props.listing.serialNumber} </p>
                     </div> 
-                    <p className="listing-price"> {this.props.listing.price} </p>
+                    <p className="listing-price"> {this.props.listing.price} <span className="currency">AUD</span> </p>
                 </div>
                 <div className="list-content">
                     <p> {this.props.listing.specs} </p>
