1d3e525f9e4f9bf585f3e2567c74a3919b61c4f6
Naesa Orlina <norlina@thoughtworks.com>
2018-01-24 16:58:07 +1000

    NO/EKK/DC: Rename listing to auction part2

--- src/auctions/Auction.js
+++ src/auctions/Auction.js
@@ -4,24 +4,24 @@ import logo from './portraitImage.jpg';
 export default class Auction extends Component {
     render() {
         return (
-            <div className="col-md-5 listing">
-                <div className="listing-upper">
-                    <div className="listing-heading">
-                        <img src={logo} className="listing-image"/>
-                        <h2> {this.props.listing.title} </h2>
-                        <p> {this.props.listing.serialNumber} </p>
+            <div className="col-md-5 auction">
+                <div className="auction-upper">
+                    <div className="auction-heading">
+                        <img src={logo} className="auction-image"/>
+                        <h2> {this.props.auction.title} </h2>
+                        <p> {this.props.auction.serialNumber} </p>
                     </div> 
-                    <p className="listing-price"> {this.props.listing.price} <span className="currency">AUD</span> </p>
+                    <p className="auction-price"> {this.props.auction.price} <span className="currency">AUD</span> </p>
                 </div>
-                <div className="list-content">
+                <div className="auction-content">
                 <h4>Specs</h4>
-                    <p> {this.props.listing.specs} </p>
+                    <p> {this.props.auction.specs} </p>
                     <h4>Condition</h4>
-                    <p> {this.props.listing.condition} </p>
+                    <p> {this.props.auction.condition} </p>
                     <h4>Location</h4>
-                    <p> {this.props.listing.location} </p>
+                    <p> {this.props.auction.location} </p>
                 </div>
-                <button className="listing-btn">Buy Now</button>
+                <button className="auction-btn">Buy Now</button>
             </div>
         )
     }
--- src/auctions/AuctionList.js
+++ src/auctions/AuctionList.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Auction from "./Auction"
 import './AuctionList.css';
 import axios from "axios/index";
+
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000/api';
 
 
@@ -9,11 +10,10 @@ export default class AuctionList extends Component {
 
     constructor() {
         super()
-        this.state = {listings: []}
+        this.state = {auctionList: []}
     }
 
     componentDidMount() {
-
         axios.get(BASE_URL + "/auctions")
             .then(this.setData.bind(this))
             .catch(function (error) {
@@ -23,24 +23,24 @@ export default class AuctionList extends Component {
 
     setData(response) {
         this.setState({
-            listings: response
+            auctionList: response
         })
     }
 
     render() {
         let content = "";
-        if (this.state.listings !== undefined && this.state.listings.data !== undefined) {
-            content = this.state.listings.data.data.map((listing) =>
-                (<Auction listing={listing} key={listing.id} />)
+        if (this.state.auctionList !== undefined && this.state.auctionList.data !== undefined) {
+            content = this.state.auctionList.data.data.map((auction) =>
+                (<Auction auction={auction} key={auction.id}/>)
             );
         }
 
         return (
-        <div className="container">
-            <div className="row justify-content-around">
-                {content}
+            <div className="container">
+                <div className="row justify-content-around">
+                    {content}
+                </div>
             </div>
-        </div>
 
         )
     }
