cc8299b6efd4dd6791387c3d2ffdc91c2892a242
Randolf Park <rpark@Randolphs-MacBook-Pro-2.local>
2018-01-24 10:29:52 +1000

    Add example of request backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Navbar from "./header/Navbar";
 import Listings from "./Listings/Listings"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    axios.get('http://localhost:4000/api/auctions')
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+  }
   render() {
     return (
       <div className="App">
