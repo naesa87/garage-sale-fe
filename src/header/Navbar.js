451017d384cf089a55b61f77dc2f1b455a7cc4f5
Naesa Orlina <norlina@thoughtworks.com>
2018-01-24 18:22:57 +1000

    NO/EKK/DC: add links/styling to navbar

--- src/header/Navbar.js
+++ src/header/Navbar.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.css'
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
+                    <p className="navbar-brand navbar-name">G'day Chandler</p>
                     <div className="navbar-nav ml-auto">
-                        <li className="nav-item">
+                        <li className="nav-item" id="manage-listings">
+                            <Link to={'/'} className="nav-link">Manage Listings</Link>
+                        </li>
+                        <li className="nav-item" id="create-listings">
                             <Link to={'/create-listing'} className="nav-link">Create Listing</Link>
                         </li>
                     </div>
