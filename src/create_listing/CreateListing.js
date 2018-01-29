b68d4d75b5657e9a90d034288b81213b12db8772
Archana Khanal <archana.kha@gmail.com>
2018-01-29 15:27:46 +1000

    NO/AK/DC fix form alignment

--- src/create_listing/CreateListing.js
+++ src/create_listing/CreateListing.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from "axios/index";
 import './CreateListing.css';
 import FileBase64 from 'react-file-base64';
@@ -12,21 +12,21 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000/api';
 // http://blog.revathskumar.com/2015/07/submit-a-form-with-react.html
 export default class CreateListing extends Component {
 
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
         this.state = {
-            location:"",
-            spec:"",
-            condition:"",
-            serial_number:"",
-            title:"",
-            price:0,
+            location: "",
+            spec: "",
+            condition: "",
+            serial_number: "",
+            title: "",
+            price: 0,
             files: [],
             errors: []
         };
     }
 
-    getFiles(files){
+    getFiles(files) {
         this.setState({ files: files });
         console.log(this.state.files);
     }
@@ -35,7 +35,7 @@ export default class CreateListing extends Component {
         e.preventDefault();
         var errors = this.validate();
         console.log(this.refs.title);
-        if(Object.keys(errors).length !== 0) {
+        if (Object.keys(errors).length !== 0) {
             this.setState({
                 errors: errors
             });
@@ -46,32 +46,32 @@ export default class CreateListing extends Component {
                 console.log(result)
                 alert("SAVED")
             }).catch(function (error) {
-            console.log(error);
-            alert("BOOOHOOOO")
-        });
+                console.log(error);
+                alert("BOOOHOOOO")
+            });
     }
 
     validate = () => {
-    var errors = {} 
-    if (this.state.title === "" || this.state.title.trim() === "") {
-        errors.title = "Title is required";
-    }
-    if (this.state.serial_number === "" || this.state.serial_number.trim() === "") {
-        errors.serial_number = "Serial Number is required";
-    }
-    if (this.state.price === 0 || this.state.price < 0) {
-        errors.price = "Price must be greater than 0";
-    }
-    if (this.state.condition === "" || this.state.condition.trim() === "") {
-        errors.condition = "Condition is required";
-    }
-    if (this.state.spec === "" || this.state.spec.trim() === "") {
-        errors.spec = "Spec is required";
-    }
-    if (this.state.location === "" || this.state.location.trim() === "") {
-        errors.location = "Location is required";
-    }
-    return errors;
+        var errors = {}
+        if (this.state.title === "" || this.state.title.trim() === "") {
+            errors.title = "Title is required";
+        }
+        if (this.state.serial_number === "" || this.state.serial_number.trim() === "") {
+            errors.serial_number = "Serial Number is required";
+        }
+        if (this.state.price === 0 || this.state.price < 0) {
+            errors.price = "Price must be greater than 0";
+        }
+        if (this.state.condition === "" || this.state.condition.trim() === "") {
+            errors.condition = "Condition is required";
+        }
+        if (this.state.spec === "" || this.state.spec.trim() === "") {
+            errors.spec = "Spec is required";
+        }
+        if (this.state.location === "" || this.state.location.trim() === "") {
+            errors.location = "Location is required";
+        }
+        return errors;
     }
 
     onChange = (e) => {
@@ -81,7 +81,7 @@ export default class CreateListing extends Component {
     }
 
     renderSelectedImgList = () => {
-        if(this.state.files.length > 1){
+        if (this.state.files.length > 1) {
             const imageListHtml = _.map(this.state.files, (file) => {
                 return (
                     <div> {file.name} </div>
@@ -89,7 +89,7 @@ export default class CreateListing extends Component {
             });
             return (<div className="filenames">
                 {imageListHtml}
-                </div>
+            </div>
             );
         }
     }
@@ -98,43 +98,44 @@ export default class CreateListing extends Component {
         const { location, spec, condition, serial_number, title, price, errors } = this.state;
         return (
             <div className="container">
-            <h3 className="create-listing-heading col-md-8"> Create Listing</h3>
-            <p className="create-listing-hint col-md-8"> Hint: Reference "About this Mac" on the machine you want to sell for assistance
+                <div className="row justify-content-md-center">
+                    <h3 className="create-listing-heading col-md-8"> Create Listing</h3>
+                    <p className="create-listing-hint col-md-8"> Hint: Reference "About this Mac" on the machine you want to sell for assistance
             with the following details. All fields are required.
             </p>
-                <form onSubmit={this.onSubmit.bind(this)} id="form1" noValidate>
-            
-                    <div className="form-group col-md-8">
+                </div>
+                <div className="row justify-content-md-center">
+                    <form onSubmit={this.onSubmit.bind(this)} id="form1" noValidate className="col-md-8">
 
-                        <FormControlGroup label="Title" value={title} errors={errors} change={this.onChange} name="title"/>
-                        <FormControlGroup label="Serial Number" value={serial_number} errors={errors} change={this.onChange} name="serial_number"/>
-                        <FormControlGroup label="Price" value={price} errors={errors} change={this.onChange} name="price"/>
-                        <FormControlGroup label="Specifications" value={spec} errors={errors} change={this.onChange} name="spec"/>
-                      
-                        <div className="control-group">  
-                        <div> 
-                            <label htmlFor="condition">Condition (200 characters max)</label> 
-                        </div>
-                            <div className="controls textarea_wrapper">
-                                <textarea  id="condition-text-area" onChange={this.onChange} rows="3" type="text" maxLength="200" value={condition} name="condition" id="condition" className={`form-control ${errors.condition ? "is-invalid" : ""}`}/>
+                        <div className="form-group">
+                            <FormControlGroup label="Title" value={title} errors={errors} change={this.onChange} name="title" />
+                            <FormControlGroup label="Serial Number" value={serial_number} errors={errors} change={this.onChange} name="serial_number" />
+                            <FormControlGroup label="Price" value={price} errors={errors} change={this.onChange} name="price" />
+                            <FormControlGroup label="Specifications" value={spec} errors={errors} change={this.onChange} name="spec" />
+                            <div className="control-group">
+                                <div>
+                                    <label htmlFor="condition">Condition (200 characters max)</label>
+                                </div>
+                                <div className="controls textarea_wrapper">
+                                    <textarea id="condition-text-area" onChange={this.onChange} rows="3" type="text" maxLength="200" value={condition} name="condition" id="condition" className={`form-control ${errors.condition ? "is-invalid" : ""}`} />
+                                </div>
+                            </div>
+                            <FormControlGroup label="Location" value={location} errors={errors} change={this.onChange} name="location" />
+                            <div className="control-group">
+                                <div className="file-upload">
+                                    <FileBase64 multiple={true} onDone={this.getFiles.bind(this)} data-buttonText="Your label here." className="file-upload" />
+                                </div>
+                                {this.renderSelectedImgList()}
+                            </div>
+                            <div className="post-button">
+                                <button type="submit" className="mb-4 btn btn-primary">Post Listing</button>
                             </div>
                         </div>
-                      
-                        <FormControlGroup label="Location" value={location} errors={errors} change={this.onChange} name="location"/>
-                        
-                        <div className="control-group">
-                            <div className="file-upload">
-                                 <FileBase64 multiple={ true } onDone={ this.getFiles.bind(this) } data-buttonText="Your label here." className="file-upload" />
-                            </div>
-                            {this.renderSelectedImgList()}
-                        </div>
+                    </form>
+                </div>
 
-<div className="post-button">
-                        <button type="submit" className="mb-4 btn btn-primary">Post Listing</button>
-</div>
-                    </div>
-                </form>
             </div>
+
         );
     }
 }
