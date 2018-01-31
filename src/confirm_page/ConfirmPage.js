293e6ff09e579fc0a19c0ef95ec7c0120ec97b53
Archana Khanal <archana.kha@gmail.com>
2018-01-31 17:11:58 +1000

    NO/AK: add confirm page

---
+++ src/confirm_page/ConfirmPage.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from "axios/index";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000/apasdasi';
+
+export default class ConfirmPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            submissionPassed: false
+        }
+    }
+
+    renderNotification = () => {
+        if (!this.state.submissionPassed) {
+            return (
+                <div className="alert alert-danger" role="alert"> Network Error. Please try again </div>
+            )
+        }
+    }
+
+    commitPurchase = () => {
+        const itemId = this.props.match.params.id;
+        axios.put(BASE_URL + "/auctions/" + itemId, { auction: { is_sold: true}})
+            .then((result) => {
+                this.setState({
+                    submissionPassed: true
+                });
+            }).catch((error) => {
+                console.log(error)
+                this.setState({
+                    submissionPassed:false
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderNotification()}
+                <div> Hello </div>
+                <button onClick={this.commitPurchase}> Confirm Purchase </button>
+            </div>
+        );
+    }
+
+}
